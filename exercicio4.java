1a4a124ec1a63bdc17f7cf997afc5e8ada034f41
Bianca Tainá Lino Balduino <bianca.balduino@escola.pr.gov.br>
2026-06-15 18:18:10 +0000

    acabei

--- exercicio4.java
+++ exercicio4.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
 
 public class exercicio4 {
-         public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("digita uma cor Verde, Amarelo ou Vermelho ");
+        System.out.println("Digite uma cor: Verde, Amarelo ou Vermelho");
         String cor = scanner.nextLine();
 
-        if(km >= 80){
-            System.out.println("Você foi multado!");
+        if (cor.equals("Verde")) {
+            System.out.println("Siga");
+        } else if (cor.equals("Amarelo")) {
+            System.out.println("Atenção");
+        } else if (cor.equals("Vermelho")) {
+            System.out.println("Pare!!");
         } else {
-            System.out.println("Boa viagem!");
+            System.out.println("Cor inválida");
         }
-        }
-    
+
+        scanner.close();
+    }
 }
