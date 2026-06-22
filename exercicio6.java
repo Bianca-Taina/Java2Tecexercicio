17ac79586680125cf8037390a616202c7a41f3e3
Bianca Tainá Lino Balduino <bianca.balduino@escola.pr.gov.br>
2026-06-22 17:39:29 +0000

    22

--- exercicio6.java
+++ exercicio6.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class exercicio{
+public class exercicio6{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite o valor da compra: R$ ");
